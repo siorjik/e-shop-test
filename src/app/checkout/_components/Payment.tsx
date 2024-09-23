ef98e4d1fb501465574d8af41a5b81ec18e0a412
siorjik <serlkaplun@gmail.com>
2024-09-23 14:16:26 +0300

    refactored checkout page

---
+++ src/app/checkout/_components/Payment.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+
+import Stripe from '@/components/Stripe'
+
+import { useCartContext } from '@/contexts/CartContext'
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+
+export default function Checkout() {
+  const { sum } = useCartContext()
+
+  const options = {
+    mode: 'payment' as const,
+    amount: +(sum * 100).toFixed(2),
+    currency: 'usd',
+    locale: 'en' as const,
+  }
+
+  return (
+    <>
+      <div className='w-[47%] mb-10'>
+        <h2 className='mb-10 text-3xl font-semibold'>${sum}</h2>
+        <p className='mb-3 text-lg'>Delivery:</p>
+        <p>in progress...</p>
+      </div>
+      {sum && <div className='lg:w-[47%] mb-10'>
+        <Elements stripe={stripePromise} options={options}>
+          <Stripe sum={sum} />
+        </Elements>
+      </div>}
+    </>
+  )
+}
--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,36 +1,9 @@
-'use client'
-
-import { loadStripe } from '@stripe/stripe-js'
-import { Elements } from '@stripe/react-stripe-js'
-
-import Stripe from '@/components/Stripe'
-
-import { useCartContext } from '@/contexts/CartContext'
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+import Payment from './_components/Payment'
 
 export default function Checkout() {
-  const { sum } = useCartContext()
-
-  const options = {
-    mode: 'payment' as const,
-    amount: +(sum * 100).toFixed(2),
-    currency: 'usd',
-    locale: 'en' as const,
-  }
-
   return (
     <div className='flex flex-col lg:flex-row justify-between'>
-      <div className='w-[47%] mb-10'>
-        <h2 className='mb-10 text-3xl font-semibold'>${sum}</h2>
-        <p className='mb-3 text-lg'>Delivery:</p>
-        <p>in progress...</p>
-      </div>
-      {sum && <div className='lg:w-[47%] mb-10'>
-        <Elements stripe={stripePromise} options={options}>
-          <Stripe sum={sum} />
-        </Elements>
-      </div>}
+      <Payment />
     </div>
   )
 }
