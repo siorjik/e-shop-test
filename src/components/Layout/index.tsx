55e9f8b9528857976f7f86700028d76e74b58187
siorjik <serlkaplun@gmail.com>
2025-01-28 20:31:32 +0200

    fixed padding in footer for mobile view

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -63,7 +63,7 @@ export default function Layout({ children }: { children: ReactNode }) {
             </aside>
             <div className='max-w-7xl mx-auto relative'>{children}</div>
           </div>
-          <footer className='h-[60px] py-2 px-8 flex items-center bg-sky-200 dark:bg-sky-700'>
+          <footer className='h-[60px] py-2 px-5 md:px-8 flex items-center bg-sky-200 dark:bg-sky-700'>
             <div className='w-full m-auto max-w-7xl flex justify-between items-center text-sm'>
               &copy; {new Date().getFullYear()}
               <ThemeBtn />
